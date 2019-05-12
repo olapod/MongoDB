aa25310dac4a09e521293b0791d641db3686da36
Ola Podsiadlik <alecs@op.pl>
2019-05-12 21:12:21 +0200

    Add script in package.json

--- server.js
+++ server.js
@@ -12,7 +12,8 @@
         // If not connected, return errors immediately rather than waiting for reconnect
         bufferMaxEntries: 0,
         // to eliminate MongoDB Atlas warning
-        useNewUrlParser: true
+        useNewUrlParser: true,
+        useFindAndModify: false
     };
     
     const connectWithRetry = () => {
